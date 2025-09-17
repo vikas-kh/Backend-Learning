e4333ad8173834fc11d613a2a9e1d21990add117
Vikas <vikkypermude@gmail.com>
2025-09-17 23:44:07 +0530

    learning a book management backend

--- l14/routes/todo.js
+++ l14/routes/todo.js
@@ -1,8 +1,7 @@
 import express from "express";
-import { createTodo ,getAllTodos,updateTodo,deleteTodo} from "../contraollers/todo.js";
+import { createTodo ,getAllTodos,updateTodo, deleteTodo} from "../contraollers/todo.js";
 const router=express.Router();
 
 router.route("/todo").post(createTodo).get(getAllTodos);
-router.route("/:todoId").put(updateTodo);
-router.route("/:todpId").delete(deleteTodo);
+router.route("/:todoId").put(updateTodo).delete(deleteTodo);
 export default router;  
